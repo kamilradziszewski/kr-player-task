a1b847bc528c614a90bc7f0fbc09126949d2f179
Kamil Radziszewski <contact@kamilradziszewski.com>
2020-07-22 17:52:04 +0200

    Updates

---
+++ precache-manifest.2e120cdc92e832e176af30bdb6e4db77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac432d7390100af4da69a2e2ade7d63e",
+    "url": "/kr-player-task/index.html"
+  },
+  {
+    "revision": "042b45ffdc45c09f28d5",
+    "url": "/kr-player-task/static/css/main.d18dee0f.chunk.css"
+  },
+  {
+    "revision": "e89842f5a1395764a719",
+    "url": "/kr-player-task/static/js/2.ea3487ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kr-player-task/static/js/2.ea3487ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "042b45ffdc45c09f28d5",
+    "url": "/kr-player-task/static/js/main.dd4c6e94.chunk.js"
+  },
+  {
+    "revision": "1de7f98acee4837b37e8",
+    "url": "/kr-player-task/static/js/runtime-main.9219c84c.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/kr-player-task/static/media/logo.8dad8028.svg"
+  }
+]);
