36fbcc0f66af2f981aea149c57c9fb33481be1be
Kamil Radziszewski <contact@kamilradziszewski.com>
2020-07-23 11:25:53 +0200

    Add Waveform component

--- src/features/player/Main.component.jsx
+++ src/features/player/Main.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Header from "./Header.component";
+import Waveform from "./Waveform.component";
 import NextSong from "./NextSong.component";
 
 const Main = () => (
   <main className="main">
     <Header />
+    <Waveform />
     <NextSong />
   </main>
 );
---
+++ src/features/player/Waveform.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Waveform = () => (
+  <div className="waveform">
+    <div className="waveform__image" />
+    <div className="waveform__mask" />
+  </div>
+);
+
+export default Waveform;
