458fbd7f40ab2ae2a6dfbd25714fb4327a155549
Kamil Radziszewski <contact@kamilradziszewski.com>
2020-07-24 20:10:19 +0200

    Add Waveform animation

--- src/features/player/Waveform.component.jsx
+++ src/features/player/Waveform.component.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
-const Waveform = () => (
+const Waveform = ({ currentTime, currentTrackLength }) => (
   <div className="waveform">
     <div className="waveform__image" />
-    <div className="waveform__mask" />
+    <div
+      className="waveform__mask"
+      style={{
+        width: `calc(100% - ${
+          (currentTime / currentTrackLength) * 100
+        }%)`,
+      }}
+    />
   </div>
 );
 
